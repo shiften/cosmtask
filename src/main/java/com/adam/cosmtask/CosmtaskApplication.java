86b1502601aaa4386ad0854665808014b97b0b5e
Adam <adam.bialek9@gmail.com>
2020-02-27 11:38:47 +0100

    Changed package name

--- src/main/java/com/adam/cosmtask/CosmtaskApplication.java
+++ src/main/java/com/adam/cosmtask/CosmtaskApplication.java
@@ -6,10 +6,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
-public class CosmoseApplication {
+public class CosmtaskApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CosmoseApplication.class, args);
+        SpringApplication.run(CosmtaskApplication.class, args);
     }
 
 }
